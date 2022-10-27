9bbdb73c0d54e186699197c2822a4f93c4823210
bartdurak <batq.ar@zoho.com>
2022-10-27 06:32:22 +0200

    przelacznik char

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+//łańcuch
+int main() {
+    float liczba1, liczba2, suma;
+    float iloraz;
+    char klucz;
+
+    cout << " Podaj klucz: "
+    cin >> klucz;
+    switch (klucz) {
+        case 'a':
+            cout << "Wybrales a";
+            break;
+        case 'b' :
+            cout << "Wybrales b";
+    }
+    <<endl;
+    return 0;
+}
